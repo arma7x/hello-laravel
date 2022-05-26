145a707a8d138fe1794a23fd4c1b78bdeb833353
Ahmad Malik <arma7x@live.com>
2022-05-27 01:15:03 +0800

    fix style & welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
     <div>
-        <h1 class="text-center">Hello Laravel</h1>
+        <h1 class="text-center" style="margin-top:calc(90vh/3)">Hello Laravel Livewire</h1>
     </div>
 @stop
