4ef2e0870fb17bec9ab8ee2d7c2d52899f680461
Ahmad Malik <arma7x@live.com>
2022-05-27 01:00:31 +0800

    add active link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,19 +9,12 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link{{Request::is('counter') ? ' active' : ''}}" href="/counter">Counter</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/counter">Counter</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/gallery">Gallery</a>
+            <a class="nav-link{{Request::is('gallery') ? ' active' : ''}}" href="/gallery">Gallery</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
